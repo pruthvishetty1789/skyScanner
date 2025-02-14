f452c70418277bac24443c1454ae6eaf20b16d3d
Pruthvi <146100300+pruthvishetty1789@users.noreply.github.com>
2025-02-14 11:51:10 +0530

    Add files via upload

---
+++ main/java/com/hoenscanner/model/search.java
@@ -0,0 +1,20 @@
+package com.hoenscanner.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Search {
+    @JsonProperty
+    private String city;
+
+    public Search() {
+        // Default constructor for Jackson
+    }
+
+    public Search(String city) {
+        this.city = city;
+    }
+
+    public String getCity() {
+        return city;
+    }
+}
